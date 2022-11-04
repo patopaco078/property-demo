3d0adcc6114b2953836f56c84b3364b3fcc570a9
patopaco078 <fan1patopaco@gmail.com>
2022-11-04 11:36:32 -0500

    fixed up bug clock

--- Assets/Scripts/Clock/ActivateClock.cs
+++ Assets/Scripts/Clock/ActivateClock.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(BoxCollider))]
 
 public class ActivateClock : MonoBehaviour
 {
     [SerializeField] GameObject[] gameObjectsActived;
+
+    [Header("Events")]
+    [SerializeField] UnityEvent EventsEnter;
+    [SerializeField] UnityEvent EventsExit;
 
     private void Start()
     {
@@ -20,7 +25,8 @@ public class ActivateClock : MonoBehaviour
     {
         if(other.tag == "PlayerWatch")
         {
-            for(int i = 0; i < gameObjectsActived.Length; i++)
+            EventsEnter.Invoke();
+            for (int i = 0; i < gameObjectsActived.Length; i++)
             {
                 gameObjectsActived[i].SetActive(true);
             }
@@ -32,6 +38,7 @@ public class ActivateClock : MonoBehaviour
         {
             if (other.tag == "PlayerWatch")
             {
+                EventsExit.Invoke();
                 for (int i = 0; i < gameObjectsActived.Length; i++)
                 {
                     gameObjectsActived[i].SetActive(false);
