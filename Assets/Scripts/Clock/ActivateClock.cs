e9879adab42040b715b4e287fd18acf8c2705ed4
patopaco078 <fan1patopaco@gmail.com>
2022-11-04 01:24:27 -0500

    funtional Clock

---
+++ Assets/Scripts/Clock/ActivateClock.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class ActivateClock : MonoBehaviour
+{
+    [SerializeField] GameObject[] gameObjectsActived;
+
+    private void Start()
+    {
+        for (int i = 0; i < gameObjectsActived.Length; i++)
+        {
+            gameObjectsActived[i].SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "PlayerWatch")
+        {
+            for(int i = 0; i < gameObjectsActived.Length; i++)
+            {
+                gameObjectsActived[i].SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        {
+            if (other.tag == "PlayerWatch")
+            {
+                for (int i = 0; i < gameObjectsActived.Length; i++)
+                {
+                    gameObjectsActived[i].SetActive(false);
+                }
+            }
+        }
+    }
+}
