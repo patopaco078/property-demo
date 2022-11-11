4a8e0136ccdbd80c720d63889b97d8a9d49f8ce2
patopaco078 <fan1patopaco@gmail.com>
2022-11-11 00:46:09 -0500

    alpha1.0

---
+++ Assets/Models/Character/Scripts/EventActivate.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class EventActivate :MonoBehaviour
+{
+    [SerializeField] AnimatorController controller;
+
+    [HideInInspector] public bool IsUsing = false;
+    
+    [SerializeField] bool HaveBoxCollider;
+    BoxCollider ActivateCollider;
+
+    [Header("Audio")]
+    [SerializeField] bool HaveAudio;  
+
+    [Header("Animation")]
+    [SerializeField] bool IsAnimated;
+    [SerializeField] string NameTrigger;
+    [SerializeField] Animator animator;
+
+    [Header("Moving")]
+    [SerializeField] bool IsMoving;
+    [SerializeField] Transform WaypointToMove;
+    [SerializeField] float TimeToMove;
+    [SerializeField] public Transform Character;
+
+    
+
+    private void Start()
+    {
+        if (HaveBoxCollider)
+        {
+            ActivateCollider = GetComponent<BoxCollider>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartEvent();
+            IsUsing = true;
+        }
+    }
+
+    public void SetVariables(Transform ExternalTransform, Animator ExternalAnimator)
+    {
+        animator = ExternalAnimator;
+        Character = ExternalTransform;
+    }
+
+    public void StartEvent()
+    {
+        if (IsAnimated)
+            Animate();
+        if (IsMoving)
+            moveCharacter();
+        if (HaveAudio)
+            StartAudio();
+        controller.NextEvent();
+        ActivateCollider.enabled = false;
+    }
+
+    private void moveCharacter()
+    {
+        Character.DOMove(WaypointToMove.position, TimeToMove);
+    }
+
+    private void Animate()
+    {
+        animator.SetTrigger(NameTrigger);
+    }
+
+    private void StartAudio()
+    {
+        controller.StarAudio();
+    }
+}
