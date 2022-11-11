308065df92769eb8904dd3e7fa4775285be5ba3d
patopaco078 <67531231+patopaco078@users.noreply.github.com>
2022-11-11 08:03:52 -0500

    Avance Rotation with player

---
+++ Assets/Models/Character/Scripts/WatchPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchPlayer : MonoBehaviour
+{
+    private bool CanWatch;
+
+    [SerializeField] private Transform PlayerTransform;
+
+    [SerializeField] Vector3 Direction = new Vector3(0, 0, 0);
+    float A;
+    float B;
+
+    private void Update()
+    {
+        A = PlayerTransform.position.x - gameObject.transform.position.x;
+        B = PlayerTransform.position.z - gameObject.transform.position.z;
+        Direction.y = (Mathf.Asin(A * (1 / Mathf.Sqrt((A * A) * (B * B))))) * 180 / Mathf.PI;
+        Debug.Log(Mathf.Sqrt((A * A) * (B * B)));
+    }
+
+    public void StartWatch()
+    {
+        CanWatch = true;
+    }
+}
